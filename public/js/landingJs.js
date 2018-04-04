17c94e1801d8b45e4bedb046470c5ac494cc4c0e
Redzepi <gazmendredzepi94@gmail.com>
2018-04-04 02:07:52 +0200

    services layout

--- public/js/landingJs.js
+++ public/js/landingJs.js
@@ -10,4 +10,72 @@ $(document).ready(function(){
         $("#landing").css("transform","scale(1)");
     });
 
+    /* tab mouseovers */
+    $("#tab-one").mouseenter(function(){
+        
+        $("#tab-one").css("transform","scale(1.1)");
+        $("#services").css("background","url('img/branding_bkg_1.jpg') center fixed no-repeat");
+        $("#services").css("-webkit-background-size","cover");
+        $("#services").css("-moz-background-size","cover");
+        $("#services").css("-o-background-size","cover");
+        $("#services").css("background-size","cover");
+    });
+
+    $("#tab-one").mouseleave(function(){
+        
+        $("#tab-one").css("transform","scale(1)");
+        $("#services").css("background","none");
+    });
+
+    $("#tab-two").mouseenter(function(){
+        
+        $("#tab-two").css("transform","scale(1.1)");
+        $("#services").css("background","url('img/design_bkg.jpg') center fixed no-repeat");
+        $("#services").css("-webkit-background-size","cover");
+        $("#services").css("-moz-background-size","cover");
+        $("#services").css("-o-background-size","cover");
+        $("#services").css("background-size","cover");
+       
+    });
+
+    $("#tab-two").mouseleave(function(){
+        
+        $("#tab-two").css("transform","scale(1)");
+        $("#services").css("background","none");
+    });
+
+    $("#tab-three").mouseenter(function(){
+        
+        $("#tab-three").css("transform","scale(1.1)");
+        $("#services").css("background","url('img/seo_bkg.jpg') center fixed no-repeat");
+        $("#services").css("-webkit-background-size","cover");
+        $("#services").css("-moz-background-size","cover");
+        $("#services").css("-o-background-size","cover");
+        $("#services").css("background-size","cover");
+    });
+
+    $("#tab-three").mouseleave(function(){
+        
+        $("#tab-three").css("transform","scale(1)");
+        $("#services").css("background","none");
+    });
+
+    $("#tab-four").mouseenter(function(){
+        
+        $("#tab-four").css("transform","scale(1.1)");
+        $("#services").css("background","url('img/social_bkg.jpg') center fixed no-repeat");
+        $("#services").css("-webkit-background-size","cover");
+        $("#services").css("-moz-background-size","cover");
+        $("#services").css("-o-background-size","cover");
+        $("#services").css("background-size","cover");
+    });
+
+    $("#tab-four").mouseleave(function(){
+        
+        $("#tab-four").css("transform","scale(1)");
+        $("#services").css("background","none");
+    });
+
+    
+
 });
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -95,11 +95,64 @@
 <!-- services section -->
 <section id="services">
 
-    <div class="row">
+    <div class="row extra-padding-25">
 
         <div class="col-md-12">
 
-            <h3>Services</h3>
+            <h3 class="text-center">Services</h3>
+
+            <div class="row position-middle-15"><!-- sub row start -->
+
+                <div class="col-md-3 mx-auto text-center bbd-tab" id="tab-one">
+                    <div class="card" style="width: 18rem;">
+                        <img class="card-img-top" src="{{asset('img/branding_bkg_1.jpg')}}" alt="Card image cap">
+                        <div class="card-body">
+                            <h6 class="card-title">Branding &amp; Logo</h6>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-3 mx-auto text-center bbd-tab" id="tab-two">
+                    <div class="card" style="width: 18rem;">
+                        <img class="card-img-top" src="{{asset('img/design_bkg.jpg')}}" alt="Card image cap">
+                        <div class="card-body">
+                            <h6 class="card-title" id="header-tab-two">Web Design &amp; Development</h6>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-3 mx-auto text-center bbd-tab" id="tab-three">
+                    <div class="card" style="width: 18rem;">
+                        <img class="card-img-top" src="{{asset('img/seo_bkg.jpg')}}" alt="Card image cap">
+                        <div class="card-body">
+                            <h6 class="card-title">SEO Optimization</h6>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-3 mx-auto text-center bbd-tab" id="tab-four">
+                    <div class="card" style="width: 18rem;">
+                        <img class="card-img-top" src="{{asset('img/social_bkg.jpg')}}" alt="Card image cap">
+                        <div class="card-body">
+                            <h6 class="card-title">Social Media Managing</h6>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-12 text-center extra-padding-50">
+
+                    <h5 class="text-center font-italic">We prefer personalized and excellent client experience</h5>
+                </div>
+
+            </div><!-- sub row end -->
         </div>
 
     </div>
@@ -110,4 +163,5 @@
 
 @section('customscripts')
 <script src="{{asset('js/landingJs.js')}}"></script>
+
 @endsection
